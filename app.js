ee54dd29e764739ddaf89f650739e047b76e2332
Vishnu <vishnu_venugopal@live.com>
2017-07-14 18:50:27 +0000

    Added Delete Route, redirecting users once button is pushed

--- app.js
+++ app.js
@@ -86,6 +86,18 @@ app.put("/blogs/:id", function(req, res){
     })
 })
 
+// DELETE ROUTE
+app.delete("/globs/:id", function(req, res){
+    //destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs")
+        } else {
+            res.redirect("/blogs")
+        }
+    })
+})
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('SERVER IS RUNNING!')
